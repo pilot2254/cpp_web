4ddcd2d5ad8a719c0999aea36173b183d396d9e5
Michal Flaška <109202734+pilot2254@users.noreply.github.com>
2026-02-25 20:20:14 +0100

    learn basics of httplib

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include "httplib.h"
+using namespace httplib;
+
+int main()
+{
+        Server svr;
+
+        svr.Get("/", [](const Request &, Response &res)
+                { res.set_content("<h1>yo from c++</h1>", "text/html"); });
+
+        svr.listen("0.0.0.0", 8080);
+}
+
+// g++ main.cpp -std=c++17 -pthread
+// or
+// g++ main.cpp -std=c++17 -pthread -lws2_32
+//      (using winsocket -> ws2_32.lib)
+
+// localhost:8080
